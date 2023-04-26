31f161e468763b101741cb1dc193d8fd497f2a44
wata330 <61071420+wata330@users.noreply.github.com>
2023-04-26 11:22:48 +0900

    Add files via upload

---
+++ keyboards/numpad_like/keymaps/via/keymap.c
@@ -0,0 +1,32 @@
+#include QMK_KEYBOARD_H
+
+const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
+    [0] = LAYOUT(
+        KC_NO,       KC_KP_7,  KC_KP_8,   KC_KP_9,   KC_BACKSPACE,
+        KC_NO,       KC_KP_4,  KC_KP_5,   KC_KP_6,   KC_KP_MINUS,
+        KC_SLASH,    KC_KP_1,  KC_KP_2,   KC_KP_3,   KC_KP_PLUS,
+        KC_ASTERISK, KC_KP_0,  KC_NO,     KC_KP_DOT, KC_KP_ENTER
+    ),
+
+    [1] = LAYOUT(
+        KC_NO,    KC_NO,  KC_NO,   KC_NO,   KC_NO,
+        KC_NO,    KC_NO,  KC_NO,   KC_NO,   KC_NO,
+        KC_NO,    KC_NO,  KC_NO,   KC_NO,   KC_NO,
+        KC_NO,    KC_NO,  KC_NO,   KC_NO,   KC_NO
+    ),
+
+    [2] = LAYOUT(
+        KC_NO,    KC_NO,  KC_NO,   KC_NO,   KC_NO,
+        KC_NO,    KC_NO,  KC_NO,   KC_NO,   KC_NO,
+        KC_NO,    KC_NO,  KC_NO,   KC_NO,   KC_NO,
+        KC_NO,    KC_NO,  KC_NO,   KC_NO,   KC_NO
+    ),
+
+    [3] = LAYOUT(
+        KC_NO,    KC_NO,  KC_NO,   KC_NO,   KC_NO,
+        KC_NO,    KC_NO,  KC_NO,   KC_NO,   KC_NO,
+        KC_NO,    KC_NO,  KC_NO,   KC_NO,   KC_NO,
+        KC_NO,    KC_NO,  KC_NO,   KC_NO,   KC_NO
+    )
+
+};
